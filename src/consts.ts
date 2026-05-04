b3c6e9d9885812cde389f733857a88708588f289
kamplom <476.1492.1789m@gmail.com>
2026-05-04 12:29:58 +0200

    change page namchange page name

--- src/consts.ts
+++ src/consts.ts
@@ -1,7 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Static Dispatch';
+export const SITE_TITLE = "kamplom's blog";
 export const SITE_DESCRIPTION =
 	'A personal blog styled like an early web relic, rebuilt for modern reading comfort.';
 export const SITE_URL = 'https://kamplom.me';
